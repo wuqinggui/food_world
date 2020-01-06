ce36e551e91478bae85037fd8c91eaf0e8e54071
1461433958 <30288223+1461433958@users.noreply.github.com>
2020-01-06 18:35:45 +0800

    menu菜谱页（未写完）

--- pages/menu.vue
+++ pages/menu.vue
@@ -1,8 +1,97 @@
 <template>
-    <div class="foods-container">
-        <div class="">
-            <!-- 内容 -->
-            我是食材页面1111
+    <!-- 菜谱页内容 -->
+    <div class="menu-container box-container">
+        <div class="menu-header">
+            <el-tabs v-model="activeName" @tab-click="handleClick">
+                <el-tab-pane label="精选" name="first">
+                    <el-row :gutter="20" type="flex" justify="space-between">
+                        <el-col>
+                            <div class="con-table">
+                                <div class="type-row">
+                                    <div class="type-row-tit">常见菜式</div>
+                                    <ol :class="{'height-auto': isHeight}">
+                                        <li v-for='(item,index) in foodList' :key="index">
+                                            <span>{{item}}</span>
+                                        </li>
+                                    </ol>
+                                    <div class="more">
+                                        <i @click="arrwTop(1)" v-if="isHeight" class="icon-arrow arrow-top"></i>
+                                        <i @click="arrwBottom(1)" v-else class="icon-arrow arrow-bottom"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </el-col>
+                    </el-row>
+                    <el-row :gutter="20" type="flex" justify="space-between">
+                        <el-col>
+                            <div class="con-table">
+                                <div class="type-row">
+                                    <div class="type-row-tit">常见菜式</div>
+                                    <ol :class="{'height-auto': isHeight}">
+                                        <li v-for='(item,index) in foodList' :key="index">
+                                            <span>{{item}}</span>
+                                        </li>
+                                    </ol>
+                                    <div class="more">
+                                        <i @click="arrwTop(1)" v-if="isHeight" class="icon-arrow arrow-top"></i>
+                                        <i @click="arrwBottom(1)" v-else class="icon-arrow arrow-bottom"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </el-col>
+                    </el-row>
+                    <el-row :gutter="20" type="flex" justify="space-between">
+                        <el-col>
+                            <div class="con-table">
+                                <div class="type-row">
+                                    <div class="type-row-tit">常见菜式</div>
+                                    <ol :class="{'height-auto': isHeight}">
+                                        <li v-for='(item,index) in foodList' :key="index">
+                                            <span>{{item}}</span>
+                                        </li>
+                                    </ol>
+                                    <div class="more">
+                                        <i @click="arrwTop(1)" v-if="isHeight" class="icon-arrow arrow-top"></i>
+                                        <i @click="arrwBottom(1)" v-else class="icon-arrow arrow-bottom"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </el-col>
+                    </el-row>
+                    <el-row :gutter="20" type="flex" justify="space-between">
+                        <el-col>
+                            <div class="con-table">
+                                <div class="type-row">
+                                    <div class="type-row-tit">常见菜式</div>
+                                    <ol :class="{'height-auto': isHeight}">
+                                        <li v-for='(item,index) in foodList' :key="index">
+                                            <span>{{item}}</span>
+                                        </li>
+                                    </ol>
+                                    <div class="more">
+                                        <i @click="arrwTop(1)" v-if="isHeight" class="icon-arrow arrow-top"></i>
+                                        <i @click="arrwBottom(1)" v-else class="icon-arrow arrow-bottom"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </el-col>
+                    </el-row>
+                    <a class="lookAll">查看全部 </a>
+                    <div class="menu-body">
+                        <h1>精选</h1>
+                    </div>
+                    <div class="boutiqueList">
+                        <ul>
+                            <li v-for="(item, index) in foodObj" :key="index">
+                                <div class="img"><img :src="item.url" alt=""></div>
+                                <p>{{item.name}}</p>
+                            </li>
+                        </ul>
+                    </div>
+                </el-tab-pane>
+                <el-tab-pane label="分类" name="second">分类</el-tab-pane>
+                <el-tab-pane label="食材" name="third">食材</el-tab-pane>
+            </el-tabs>
         </div>
         <!-- 加载动效 -->
         <Loading v-if="showLoading"></Loading>
@@ -12,11 +101,33 @@
 <script>
 import Loading from '@/components/Loading'
 export default {
-    name: 'index',
+    name: 'menus',
     data () {
         return {
             // 全屏加载
+            activeName: 'first',
             showLoading:false,
+            isHeight: false,
+            foodList: ['家常菜', '粤菜', '湘菜', '湖南菜', '川菜', '北京烤鸭'],
+            foodObj: [{
+                name: '番茄炒鸡蛋',
+                url: require('@/assets/images/food-1.jpg')
+            },{
+                name: '番茄炒鸡蛋2',
+                url: require('@/assets/images/food-2.jpg')
+            },{
+                name: '番茄炒鸡蛋3',
+                url: require('@/assets/images/food-3.jpg')
+            },{
+                name: '番茄炒鸡蛋',
+                url: require('@/assets/images/food-1.jpg')
+            },{
+                name: '番茄炒鸡蛋2',
+                url: require('@/assets/images/food-2.jpg')
+            },{
+                name: '番茄炒鸡蛋3',
+                url: require('@/assets/images/food-3.jpg')
+            }]
         }
     },
     components:{
@@ -32,7 +143,28 @@ export default {
         
     },
     methods: {
-
+        handleClick(tab, event) {
+            console.log(tab, event);
+        },
+        // 筛选箭头
+        arrwTop (type) {
+            if (type === 1) {
+                this.isHeight = false;
+            } else if (type === 2) {
+                this.is2Height = false;
+            } else {
+                this.is3Height = false;
+            }
+        },
+        arrwBottom (type) {
+            if (type === 1) {
+                this.isHeight = true;
+            } else if (type === 2) {
+                this.is2Height = true;
+            } else {
+                this.is3Height = true;
+            }
+        },
     },
     watch: {
         
