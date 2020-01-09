93fb8651817c7e2ea9df04462ae2b73a3b944270
1461433958 <30288223+1461433958@users.noreply.github.com>
2020-01-09 09:43:25 +0800

    加loadding

--- plugins/vue-lazyload.js
+++ plugins/vue-lazyload.js
@@ -3,6 +3,7 @@ import VueLazyload from 'vue-lazyload'
 
 export default () => {
     Vue.use(VueLazyload, {
+        loading: require('@/assets/images/loadding.gif'),
         preLoad: 1.3, //预加载的宽高比
         attempt: 1 //尝试加载次数
       })
